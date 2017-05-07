9719939a14c1101997facd0575a5dd4a6e74f224
Eric Kerwin <ekerwin@blackducksoftware.com>
2017-05-07 11:58:28 -0400

    improving configuration handling

--- src/main/groovy/com/blackducksoftware/integration/gradle/DependencyGatherer.groovy
+++ src/main/groovy/com/blackducksoftware/integration/gradle/DependencyGatherer.groovy
@@ -1,6 +1,9 @@
 package com.blackducksoftware.integration.gradle
 
+import java.lang.reflect.Method
+
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.ResolvedDependency
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -30,7 +33,7 @@ class DependencyGatherer {
         rootProject.allprojects.each { project ->
             if (projectFilter.shouldInclude(project.name)) {
                 project.configurations.each { configuration ->
-                    if (configurationFilter.shouldInclude(configuration.name)) {
+                    if (shouldIncludeConfiguration(configuration, configurationFilter)) {
                         configuration.resolvedConfiguration.firstLevelModuleDependencies.each { dependency ->
                             addDependencyNodeToParent(configuration.name, rootProjectNode, dependency)
                         }
@@ -40,6 +43,23 @@ class DependencyGatherer {
         }
 
         return rootProjectNode;
+    }
+
+    private boolean shouldIncludeConfiguration(Configuration configuration, ExcludedIncludedFilter configurationFilter) {
+        if (!configurationFilter.shouldInclude(configuration.name)) {
+            return false
+        }
+        try {
+            Method isCanBeResolved = Configuration.class.getMethod("isCanBeResolved")
+            boolean result = isCanBeResolved.invoke(configuration)
+            if (!result) {
+                return false
+            }
+        } catch (Exception e) {
+            //Exceptions are likely here since we are trying to invoke a method that may not exist (isCanBeResolved was added in 3.3)
+            logger.debug("Trying to invoke isCanBeResolved threw an Exception (likely not an issue): ${e.message}")
+        }
+        return true
     }
 
     private void addDependencyNodeToParent(String configurationName, DependencyNode parentDependencyNode, final ResolvedDependency resolvedDependency) {
