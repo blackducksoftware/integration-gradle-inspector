1a497b40f6385d01c209755cfa719d7220067971
Eric Kerwin <ekerwin@blackducksoftware.com>
2017-06-18 19:26:34 -0400

    using latest bdio and not using the builder anymore

--- src/main/groovy/com/blackducksoftware/integration/gradle/DependencyGatherer.groovy
+++ src/main/groovy/com/blackducksoftware/integration/gradle/DependencyGatherer.groovy
@@ -9,7 +9,6 @@ import org.gradle.api.artifacts.ResolvedDependency
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-import com.blackducksoftware.integration.hub.bdio.simple.DependencyNodeBuilder
 import com.blackducksoftware.integration.hub.bdio.simple.model.DependencyNode
 import com.blackducksoftware.integration.hub.bdio.simple.model.externalid.MavenExternalId
 import com.blackducksoftware.integration.util.ExcludedIncludedFilter
@@ -18,7 +17,6 @@ class DependencyGatherer {
     private final Logger logger = LoggerFactory.getLogger(DependencyGatherer.class)
 
     private Set<String> alreadyAddedIds
-    private DependencyNodeBuilder dependencyNodeBuilder
 
     DependencyNode getFullyPopulatedRootNode(final Project rootProject, String excludedProjectNames, String includedProjectNames, String excludedConfigurationNames, String includedConfigurationNames) {
         /**
@@ -34,7 +32,6 @@ class DependencyGatherer {
         ExcludedIncludedFilter projectFilter = new ExcludedIncludedFilter(excludedProjectNames, includedProjectNames)
         ExcludedIncludedFilter configurationFilter = new ExcludedIncludedFilter(excludedConfigurationNames, includedConfigurationNames)
         alreadyAddedIds = new HashSet<>()
-        dependencyNodeBuilder = new DependencyNodeBuilder(rootProjectNode)
 
         rootProject.allprojects.each { project ->
             if (projectFilter.shouldInclude(project.name)) {
@@ -85,7 +82,7 @@ class DependencyGatherer {
 
         def mavenExternalId = new MavenExternalId(group, name, version)
         def dependencyNode = new DependencyNode(name, version, mavenExternalId)
-        dependencyNodeBuilder.addChildNodeWithParents(dependencyNode, [parentDependencyNode])
+        parentDependencyNode.children.add(dependencyNode)
         if (alreadyAddedIds.add(mavenExternalId.createDataId())) {
             for (ResolvedDependency child : resolvedDependency.getChildren()) {
                 addDependencyNodeToParent(dependencyNode, child)
