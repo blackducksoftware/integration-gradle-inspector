1cd982e76800367119b79283d1063a861fa2d596
jrichard <jrichard@blackducksoftware.com>
2017-09-15 14:52:33 -0400

    Another attempt at multi-threading

--- src/main/groovy/com/blackducksoftware/integration/gradle/DependencyGatherer.groovy
+++ src/main/groovy/com/blackducksoftware/integration/gradle/DependencyGatherer.groovy
@@ -8,6 +8,7 @@ import java.util.concurrent.Executors
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.ResolvedConfiguration
+import org.gradle.api.artifacts.ResolvedDependency
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -28,65 +29,109 @@ class DependencyGatherer {
 
 
     void createAllCodeLocationFiles(final Project rootProject, String excludedProjectNames, String includedProjectNames, String excludedConfigurationNames, String includedConfigurationNames, File outputDirectory) {
-        ExcludedIncludedFilter projectFilter = new ExcludedIncludedFilter(excludedProjectNames, includedProjectNames)
-        ExcludedIncludedFilter configurationFilter = new ExcludedIncludedFilter(excludedConfigurationNames, includedConfigurationNames)
+        multiThreadedProjects(rootProject, excludedProjectNames, includedProjectNames, excludedConfigurationNames, includedConfigurationNames, outputDirectory)
+    }
 
-        String projectGroup = ''
-        String projectName = ''
-        String projectVersionName = ''
-        rootProject.allprojects.each { project ->
-            if (projectFilter.shouldInclude(project.name)) {
-                logger.info("Processing Project ${project.name}")
-                def group = project.group.toString()
-                def name = project.name.toString()
-                def version = project.version.toString()
-                if (!projectGroup) {
-                    projectGroup = group
+    void multiThreadedProjects(final Project rootProject, String excludedProjectNames, String includedProjectNames, String excludedConfigurationNames, String includedConfigurationNames, File outputDirectory) {
+        try {
+            ExcludedIncludedFilter projectFilter = new ExcludedIncludedFilter(excludedProjectNames, includedProjectNames)
+            ExcludedIncludedFilter configurationFilter = new ExcludedIncludedFilter(excludedConfigurationNames, includedConfigurationNames)
+
+            String projectGroup = rootProject.group.toString()
+            String projectName = rootProject.name.toString()
+            String projectVersionName = rootProject.version.toString()
+            CountDownLatch latch = new CountDownLatch(rootProject.allprojects.size())
+            logger.info("Processing ${rootProject.allprojects.size()} projects")
+
+            int processors = Runtime.getRuntime().availableProcessors();
+            logger.info("Processors ${processors}")
+            ExecutorService executor = Executors.newFixedThreadPool(processors);
+
+
+            rootProject.allprojects.each { project ->
+                if (projectFilter.shouldInclude(project.name)) {
+                    ProjectProcessor projectProcessor = new ProjectProcessor(latch, project, configurationFilter, outputDirectory,  projectGroup,  projectName,  projectVersionName,  gson)
+                    executor.submit(projectProcessor);
                 }
-                if (!projectName) {
-                    projectName = name
-                }
-                if (!projectVersionName) {
-                    projectVersionName = version
-                }
-                DependencyNode projectNode = new DependencyNode(name, version, new MavenExternalId(group, name, version))
-
-                CountDownLatch latch = new CountDownLatch(project.configurations.size())
-
-                int processors = Runtime.getRuntime().availableProcessors();
-                ExecutorService executor = Executors.newFixedThreadPool(processors);
-
-                List<ConfigurationProcessor> configurationProcessors = new ArrayList<>()
-
-                DependencyNodeBuilder builder = new DependencyNodeBuilder(projectNode)
-
-                project.configurations.each { configuration ->
-                    ResolvedConfiguration resolvedConfiguration = resolveConfiguration(configuration, configurationFilter)
-                    if (resolvedConfiguration != null) {
-                        ConfigurationProcessor configurationProcessor = new ConfigurationProcessor(latch, resolvedConfiguration, name, configuration.name, builder)
-                        configurationProcessors.add(configurationProcessor)
-                        executor.submit(configurationProcessor);
-                    }
-                }
-                try {
-                    latch.await();
-                } catch (InterruptedException e) {
-                    logger.error(e.getMessage(), e)
-                }
-
-                logger.info("Finished processing the configurations for Project ${name}")
-
-                File outputFile = new File(outputDirectory, "${group}_${name}_detectCodeLocation.json")
-                if (outputFile.exists()) {
-                    outputFile.delete()
-                }
-                DetectCodeLocation codeLocation = new DetectCodeLocation(BomToolType.GRADLE, project.getProjectDir().getAbsolutePath(), projectName, projectVersionName,
-                        new MavenExternalId(projectGroup, projectName, projectVersionName),builder.root.children)
-
-                JsonWriter jsonWriter = gson.newJsonWriter(new BufferedWriter(new FileWriter(outputFile)))
-                gson.toJson(codeLocation, DetectCodeLocation.class, jsonWriter)
-                jsonWriter.close()
             }
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                logger.error(e.getMessage(), e)
+            }
+
+            logger.info("Finished processing the all Projects")
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e)
+            throw e;
+        }
+    }
+
+    void multiThreadedConfigurations(final Project rootProject, String excludedProjectNames, String includedProjectNames, String excludedConfigurationNames, String includedConfigurationNames, File outputDirectory) {
+        try {
+            ExcludedIncludedFilter projectFilter = new ExcludedIncludedFilter(excludedProjectNames, includedProjectNames)
+            ExcludedIncludedFilter configurationFilter = new ExcludedIncludedFilter(excludedConfigurationNames, includedConfigurationNames)
+
+            String projectGroup = ''
+            String projectName = ''
+            String projectVersionName = ''
+            rootProject.allprojects.each { project ->
+                if (projectFilter.shouldInclude(project.name)) {
+                    logger.info("Processing Project ${project.name}")
+                    def group = project.group.toString()
+                    def name = project.name.toString()
+                    def version = project.version.toString()
+                    if (!projectGroup) {
+                        projectGroup = group
+                    }
+                    if (!projectName) {
+                        projectName = name
+                    }
+                    if (!projectVersionName) {
+                        projectVersionName = version
+                    }
+                    DependencyNode projectNode = new DependencyNode(name, version, new MavenExternalId(group, name, version))
+
+                    CountDownLatch latch = new CountDownLatch(project.configurations.size())
+
+                    int processors = Runtime.getRuntime().availableProcessors();
+                    ExecutorService executor = Executors.newFixedThreadPool(processors);
+
+                    List<ConfigurationProcessor> configurationProcessors = new ArrayList<>()
+
+                    DependencyNodeBuilder builder = new DependencyNodeBuilder(projectNode)
+
+                    project.configurations.each { configuration ->
+                        ResolvedConfiguration resolvedConfiguration = resolveConfiguration(configuration, configurationFilter)
+                        if (resolvedConfiguration != null) {
+                            ConfigurationProcessor configurationProcessor = new ConfigurationProcessor(latch, resolvedConfiguration, name, configuration.name, builder)
+                            configurationProcessors.add(configurationProcessor)
+                            executor.submit(configurationProcessor);
+                        }
+                    }
+                    try {
+                        latch.await();
+                    } catch (InterruptedException e) {
+                        logger.error(e.getMessage(), e)
+                    }
+
+                    logger.info("Finished processing the configurations for Project ${name}")
+
+                    File outputFile = new File(outputDirectory, "${group}_${name}_detectCodeLocation.json")
+                    if (outputFile.exists()) {
+                        outputFile.delete()
+                    }
+                    DetectCodeLocation codeLocation = new DetectCodeLocation(BomToolType.GRADLE, project.getProjectDir().getAbsolutePath(), projectName, projectVersionName,
+                            new MavenExternalId(projectGroup, projectName, projectVersionName),builder.root.children)
+
+                    JsonWriter jsonWriter = gson.newJsonWriter(new BufferedWriter(new FileWriter(outputFile)))
+                    gson.toJson(codeLocation, DetectCodeLocation.class, jsonWriter)
+                    jsonWriter.close()
+                }
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e)
+            throw e;
         }
     }
 
@@ -120,6 +165,24 @@ class DependencyGatherer {
         builder.addParentNodeWithChildren(parent, children)
         for (DependencyNode child : children) {
             addDependencyNodesToBuilder(builder, child, child.children)
+        }
+    }
+
+    private void addDependencyNodeToParent(DependencyNodeBuilder builder, DependencyNode parentDependencyNode, final ResolvedDependency resolvedDependency) {
+        def group = resolvedDependency.moduleGroup
+        def name = resolvedDependency.moduleName
+        def version = resolvedDependency.moduleVersion
+
+        def mavenExternalId = new MavenExternalId(group, name, version)
+        def dependencyNode = new DependencyNode(name, version, mavenExternalId)
+
+        if(parentDependencyNode == null) {
+            builder.addParentNodeWithChildren(builder.root, [dependencyNode])
+        } else {
+            builder.addParentNodeWithChildren(parentDependencyNode, [dependencyNode])
+        }
+        for (ResolvedDependency child : resolvedDependency.getChildren()) {
+            addDependencyNodeToParent(builder, dependencyNode, child)
         }
     }
 }
---
+++ src/main/groovy/com/blackducksoftware/integration/gradle/ProjectProcessor.groovy
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 2017 Black Duck Software Inc.
+ * http://www.blackducksoftware.com/
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Black Duck Software ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Black Duck Software.
+ */
+package com.blackducksoftware.integration.gradle
+
+import java.lang.reflect.Method
+import java.util.concurrent.CountDownLatch
+
+import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.ResolvedConfiguration
+import org.gradle.api.artifacts.ResolvedDependency
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import com.blackducksoftware.integration.hub.bdio.simple.model.DependencyNode
+import com.blackducksoftware.integration.hub.bdio.simple.model.externalid.MavenExternalId
+import com.blackducksoftware.integration.hub.detect.model.BomToolType
+import com.blackducksoftware.integration.hub.detect.model.DetectCodeLocation
+import com.blackducksoftware.integration.util.ExcludedIncludedFilter
+import com.google.gson.Gson
+import com.google.gson.stream.JsonWriter
+
+class ProjectProcessor implements Runnable {
+    private final Logger logger = LoggerFactory.getLogger(ProjectProcessor.class)
+    private final CountDownLatch latch
+    private final Project project
+
+    private final ExcludedIncludedFilter configurationFilter
+    private final File outputDirectory
+
+    private final  String projectGroup
+    private final  String projectName
+    private final  String projectVersionName
+
+    private final Gson gson
+
+    public ProjectProcessor(CountDownLatch latch, Project project, ExcludedIncludedFilter configurationFilter, File outputDirectory, String projectGroup, String projectName, String projectVersionName, Gson gson) {
+        this.latch = latch
+        this.project = project
+        this.configurationFilter=configurationFilter
+        this.outputDirectory = outputDirectory
+        this.gson=gson
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            logger.info("Processing Project ${project.name}, Configurations ${project.configurations.size()}")
+
+            def group = project.group.toString()
+            def name = project.name.toString()
+            def version = project.version.toString()
+            DependencyNode projectNode = new DependencyNode(name, version, new MavenExternalId(group, name, version))
+
+
+            //DependencyNodeBuilder builder = new DependencyNodeBuilder(projectNode)
+
+            project.configurations.each { configuration ->
+                ResolvedConfiguration resolvedConfiguration = resolveConfiguration(configuration, configurationFilter)
+                if (resolvedConfiguration != null) {
+                    logger.info("Processing Project ${name}, Configuration ${configuration.name}")
+                    resolvedConfiguration.firstLevelModuleDependencies.each { dependency ->
+                        addDependencyNodeToParent(projectNode, dependency)
+                    }
+                }
+            }
+
+            logger.info("Finished processing the configurations for Project ${name}")
+
+            File outputFile = new File(outputDirectory, "${group}_${name}_detectCodeLocation.json")
+            if (outputFile.exists()) {
+                outputFile.delete()
+            }
+            DetectCodeLocation codeLocation = new DetectCodeLocation(BomToolType.GRADLE, project.getProjectDir().getAbsolutePath(), projectName, projectVersionName,
+                    new MavenExternalId(projectGroup, projectName, projectVersionName),projectNode.children)
+
+            JsonWriter jsonWriter = gson.newJsonWriter(new BufferedWriter(new FileWriter(outputFile)))
+            gson.toJson(codeLocation, DetectCodeLocation.class, jsonWriter)
+            jsonWriter.close()
+
+            logger.info("Finished Project code location file ${outputFile.getAbsolutePath()}")
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e)
+            throw e;
+        } finally {
+            latch.countDown()
+        }
+    }
+
+    private ResolvedConfiguration resolveConfiguration(Configuration configuration, ExcludedIncludedFilter configurationFilter) {
+        if (!configurationFilter.shouldInclude(configuration.name)) {
+            return null
+        }
+        try {
+            Method isCanBeResolved = Configuration.class.getMethod("isCanBeResolved")
+            boolean result = isCanBeResolved.invoke(configuration)
+            if (result) {
+                return configuration.resolvedConfiguration
+            } else {
+                return null
+            }
+        } catch (Exception e) {
+            //Exceptions are likely here since we are trying to invoke a method that may not exist (isCanBeResolved was added in 3.3)
+            logger.debug("Trying to invoke isCanBeResolved threw an Exception (likely not an issue): ${e.message}")
+        }
+
+        try {
+            return configuration.resolvedConfiguration
+        } catch (Exception e) {
+            //Exceptions are unlikely here since there should not be any configurations that can not be resolved without the isCanBeResolved method
+            logger.error("Tried to resolve a configuration that can't be resolved and the isCanBeResolved method doesn't appear to exist: ${e.message}")
+        }
+        return null
+    }
+
+    private void addDependencyNodeToParent(DependencyNode parentDependencyNode, final ResolvedDependency resolvedDependency) {
+        def group = resolvedDependency.moduleGroup
+        def name = resolvedDependency.moduleName
+        def version = resolvedDependency.moduleVersion
+
+        def mavenExternalId = new MavenExternalId(group, name, version)
+        def dependencyNode = new DependencyNode(name, version, mavenExternalId)
+
+        parentDependencyNode.children.add(dependencyNode)
+
+
+        for (ResolvedDependency child : resolvedDependency.getChildren()) {
+            addDependencyNodeToParent(dependencyNode, child)
+        }
+    }
+}
