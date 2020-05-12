330f3d593f45cc0bcd25c95071035dc92051d22a
Eric Kerwin <ekerwin@synopsys.com>
2020-05-12 14:25:05 -0400

    adding test

---
+++ src/test/java/com/blackducksoftware/integration/gradle/DummyTest.java
@@ -0,0 +1,11 @@
+package com.blackducksoftware.integration.gradle;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+    @Test
+    public void testDummy() {
+        // Dummy test to pass the build until other tests exist
+    }
+
+}
